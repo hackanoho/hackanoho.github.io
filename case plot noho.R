0f1db9b54408ff3ade9bc4cb77e7ea89c14c131c
David Welch <welchd@users.noreply.github.com>
2023-03-26 16:36:35 +1300

    Update case plot noho.R

--- case plot noho.R
+++ case plot noho.R
@@ -23,15 +23,15 @@ doublings <- data.frame(time = c(4, 2,  10/7, 1, 5/7, 4/7, 3/7),
 doublings$logtime <- 2^(1/doublings$time)
 gooddoublings <-  doublings$time > max(z$averatio)
 goodhalvings <-  doublings$time < min(z$averatio)
-doublings = doublings[1:3,]
+doublings <- doublings[1:3,]
 
-labelxoffset = 4
+labelxoffset <- 4
 
 breaks <- c(0.5*10^(-10:10) ,10^(-10:10)) 
 minor_breaks <- rep(1:9, 21)*(10^rep(-10:10, each=9))
 
 negdouble = doublings[1:2,]
-p1 = qplot(data = z, x = Report_Date, ratio) + 
+p1 = qplot(data = z, x = Report_Date, ratio)+ 
   scale_y_log10(name = "Ratio of cases on day to cases a week earlier (log scale)") +
   geom_line(aes(y = averatio))+
   geom_hline(yintercept = doublings$logtime, colour = "darkred", alpha = .3)+
@@ -42,9 +42,13 @@ p1 = qplot(data = z, x = Report_Date, ratio) +
   annotate("text", label = "Cases doubling in...", x = min(z$Report_Date)+labelxoffset, y= 1.75)+
   labs(title = paste("Covid cases week on week trend - Ao/NZ"),  x = "Report date")
 
-p2 = qplot(data = z, x = Report_Date, cases) + 
+
+  
+ p2 =  qplot(data = z, x = Report_Date, cases) + 
   scale_y_log10(name = "Cases (log scale)", breaks = breaks, minor_breaks  = minor_breaks) +
   geom_line(aes(y = avecases))+
   labs(title = paste("Covid daily cases with 7-day rolling average - Ao/NZ to",today()), x = "Report date")
 
 plot_grid(p2, p1)
+
+ggsave(paste(today(),"aocases.jpg",sep = ""), width = 400, height = 200, units = "mm")
